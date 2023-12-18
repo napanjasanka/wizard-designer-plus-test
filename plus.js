7de9f44beac9466f03407dc9fd8b5ddd81400bfc
napanjasanka <153903347+napanjasanka@users.noreply.github.com>
2023-12-18 14:20:49 +0700

    Update plus.js

--- plus.js
+++ plus.js
@@ -75,12 +75,12 @@ if (document.location.href == "https://wizard-designer-pc.qa.agoda.is/deploy") {
               var columns = row.split(comma_regex); // Split by regex
               columns.forEach(function (column, column_index) {
                 var clean_column = column.replaceAll('"', ""); // Removes extra quotes
-                if (clean_column != ",") {
+                //if (clean_column != ",") {
                   table_columns +=
                     row_index == 0
                       ? "<th " + thStyle + ">" + clean_column + "</th>"
                       : "<td " + tdStyle + ">" + clean_column + "</td>";
-                }
+               // }
               });
               if (row_index == 0) {
                 table_header += "<tr>" + table_columns + "</tr>";
